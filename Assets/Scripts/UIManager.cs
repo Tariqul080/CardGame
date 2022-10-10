e27ac747ad1c2e3558532139e5a6024b57756bbe
Md.Tariqul islam <79364838+Tariqul080@users.noreply.github.com>
2022-10-10 17:27:37 +0600

    Working with player cards

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,26 @@
 using UnityEngine;
+
 namespace CallBreak {
     public class UIManager : MonoBehaviour {
+
+        [Header("Card Init")]
         [SerializeField] private Card cardFeb = null;
         [SerializeField] private Transform allCardParent = null;
         [SerializeField] private GameObject playButton = null;
         [SerializeField] private GameObject allPlayerInformation = null; 
+        
+        [Header("Card Pool")] 
+        [SerializeField] private RectTransform cardSize = null;
+        [SerializeField] private RectTransform playerCardPanel = null;
+
+        private Card[] allCards = new Card[52];
+        
+        public void ClickPlayButton() {
+            allPlayerInformation.SetActive(true);
+            playButton.SetActive(false);
+            CardMaker();
+            Invoke(nameof(ShortPlayerCards), 1f);
+        }
 
         private void CardMaker() {
             int cardType = 0, cardIndex = 0;
@@ -19,16 +35,17 @@ namespace CallBreak {
                     cardIndex = 0;
                     cardType++;
                 }
-             
+                allCards[i] = card;
             }
         }
 
-        public void ClickPlayButton() {
-            allPlayerInformation.SetActive(true);
-            playButton.SetActive(false);
-            CardMaker();
+        private void ShortPlayerCards() {
+            for(int i = 0; i < 13 ; i++) {
+                Card card = allCards[i];
+                card.transform.parent = playerCardPanel;
+            }
+            playerCardPanel.sizeDelta = new Vector2(cardSize.sizeDelta.x * 13f, playerCardPanel.sizeDelta.y);
         }
-       
     }
 }
 
